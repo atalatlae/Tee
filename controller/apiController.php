f70a6f998db79d4e1e5f471ab3f8a69ea2a1536e
Boris <atalatlae@gmail.com>
2017-03-05 20:05:24 -0300

    Tee API
    [ADD] new method UserDao->getAll() to get all users
    [ADD] new method UserDao->create() to create a user
    [ADD] new method UserDao->delete() to delete a user
    [ADD] new class apiController to manage the API request
    [ADD] new method apiController->UserAction() to manage the users
    [ADD] new methor apiController->UsersAction() to get users info

---
+++ controller/apiController.php
@@ -0,0 +1,244 @@
+<?php
+
+class   apiController
+{
+  private $username;
+  private $password;
+
+  public function __construct() {
+    $this->username = $_SERVER['PHP_AUTH_USER'];
+    $this->password = hash('sha256', $_SERVER['PHP_AUTH_PW']);
+  }
+
+  public function IndexAction() {
+    $this->verifyPermissions();
+    $this->outputAsJson(array());
+  }
+
+  public function UserAction() {
+    $param = isset($this->params[0])?$this->params[0]:null;
+
+    if ($param != '') {
+      if ($this->requestIs('GET')) {
+        $this->verifyPermissions();
+        $this->getUser($param);
+      }
+      else if ($this->requestIs('DELETE')) {
+        $this->verifyPermissions('admin');
+        $this->deleteUser($param);
+      }
+      else if ($this->requestIs('PUT')) {
+        $this->verifyPermissions('admin');
+        $this->updateUser($param);
+      }
+      else {
+        $this->methodNotAllowed();
+      }
+    }
+    else {
+      if ($this->requestIs('POST')) {
+        $this->createUser();
+      }
+      else {
+        $this->methodNotAllowed();
+      }
+    }
+
+    $this->outputAsJson(array());
+  }
+
+  public function UsersAction() {
+    if (!$this->requestIs('GET')) {
+      $this->verifyPermissions();
+      $this->methodNotAllowed();
+    }
+
+    $param = isset($this->params[0])?$this->params[0]:null;
+    if ($param == 'all') {
+      $this->verifyPermissions();
+      $this->getAllUsers();
+    }
+
+    $this->outputAsJson(array());
+  }
+
+  private function outputAsJson($jsonArray) {
+    header("Content-Type: application/json");
+    echo json_encode($jsonArray);
+    exit();
+  }
+
+  private function outputUnauthorize() {
+    header("HTTP/1.1 401 UNAUTHORIZED");
+    header("Content-Type: application/json");
+
+    $jsonArray = array('result' => 'ERROR', 'message' => 'Unauthorized access');
+    echo json_encode($jsonArray);
+    exit();
+  }
+
+  private function verifyPermissions($role = '') {
+
+    $userDao = new userDao();
+    $user = $userDao->get($this->username);
+
+    if ($user instanceof User) {
+      if ($role != '' && !$user->hasRole('admin')) {
+        $this->outputUnauthorize();
+      }
+
+      if ($user->password != $this->password) {
+        $this->outputUnauthorize();
+      }
+      return true;
+    }
+    $this->outputUnauthorize();
+  }
+
+  private function getUser($username) {
+    $userDao = new UserDao();
+    $user = $userDao->getByUsername($username);
+
+    if ($user instanceof User) {
+      $jsonArray = array(
+        'username' => $user->username,
+        'roles' => $user->roles
+      );
+    }
+    else {
+      $jsonArray = array();
+    }
+    $this->outputAsJson($jsonArray);
+  }
+
+  private function createUser() {
+    $userDao = new UserDao();
+
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $roles = $_POST['roles'];
+
+    if ($username == '' || $password == '' || count($roles) == 0) {
+      $jsonArray = array(
+        'result' => 'ERROR',
+        'message' => 'data missing'
+      );
+      $this->outputAsJson($jsonArray);
+    }
+
+    if ($userDao->exists($username)) {
+      $jsonArray = array(
+        'result' => 'ERROR',
+        'message' => 'User exists'
+      );
+      $this->outputAsJson($jsonArray);
+    }
+
+    $password = hash('sha256', $password);
+    $user = new User($username, $password, $roles);
+    $r = $userDao->create($user);
+
+    if ($r) {
+      $jsonArray = array('result' => 'OK');
+    }
+    else {
+      $jsonArray = array(
+        'result' => 'ERROR',
+        'message' => 'There was an error creating the user'
+      );
+    }
+    $this->outputAsJson($jsonArray);
+  }
+
+  private function updateUser() {
+    $userDao = new UserDao();
+
+    $username = filter_input(INPUT_GET, 'username', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_GET, 'password', FILTER_SANITIZE_STRING);
+    $roles = $_GET['roles'];
+
+    if ($username == '' || $password == '' || count($roles) == 0) {
+      $jsonArray = array(
+        'result' => 'ERROR',
+        'message' => 'data missing'
+      );
+      $this->outputAsJson($jsonArray);
+    }
+
+    if (!$userDao->exists($username)) {
+      $jsonArray = array(
+        'result' => 'ERROR',
+        'message' => 'User not exists'
+      );
+      $this->outputAsJson($jsonArray);
+    }
+
+    $tmpRoles = array();
+    foreach($roles as $r) {
+      $tmpRoles[$r] = 1;
+    }
+    $roles = $tmpRoles;
+
+    $password = hash('sha256', $password);
+    $user = new User($username, $password, $roles);
+    $r = $userDao->create($user);
+
+    if ($r) {
+      $jsonArray = array('result' => 'OK');
+    }
+    else {
+      $jsonArray = array(
+        'result' => 'ERROR',
+        'message' => 'There was an error updating the user'
+      );
+    }
+    $this->outputAsJson($jsonArray);
+  }
+
+  private function deleteUser($username) {
+    $userDao = new UserDao();
+    $r = $userDao->delete($username);
+
+    if ($r) {
+      $jsonArray = array('result' => 'OK');
+    }
+    else {
+      $jsonArray = array(
+        'result' => 'ERROR',
+        'message' => 'There was an error deleting the user'
+      );
+    }
+    $this->outputAsJson($jsonArray);
+  }
+
+  private function getAllUsers() {
+    if (!$this->requestIs('GET')) {
+      $this->methodNotAllowed();
+    }
+
+    $userDao = new UserDao();
+    $users = $userDao->getAll();
+
+    $jsonArray = array();
+    foreach($users as $u) {
+      $user = array(
+        'username' => $u->username,
+        'roles' => $u->roles
+      );
+
+      $jsonArray[$u->username] = $user;
+    }
+
+    $this->outputAsJson($jsonArray);
+  }
+
+  private function requestIs($method) {
+		$method = strtoupper($method);
+		return ($_SERVER['REQUEST_METHOD'] == $method);
+	}
+
+  private function methodNotAllowed() {
+    header("HTTP/1.0 405 METHOD NOT ALLOWED");
+    exit();
+  }
+}
--- model/UserDao.php
+++ model/UserDao.php
@@ -11,4 +11,25 @@ class UserDao extends DummyDb
   public function getByUsername($username) {
     return $this->get($username);
   }
+
+  public function getAll() {
+    return $this->storage;
+  }
+
+  public function create(User $user) {
+    $this->add($user->username, $user);
+    $this->save();
+    return true;
+  }
+
+  public function delete($username) {
+    if ($this->exists($username)) {
+      unset($this->storage[$username]);
+      $this->save();
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
